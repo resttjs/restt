4d0948551afb9e9c74db59a0aef84190d8ea7639
larkin-nz <daniel@larkin.nz>
2019-08-30 15:55:54 +0200

    Smarter URL support and better support for sending Arrays in Actions

--- bin/restt.js
+++ bin/restt.js
@@ -11,7 +11,25 @@
 try {
 
   // Import Restt-CLI and execute
-  const cli = require('restt-cli/bin/restt.js');
+  const cli = require('restt-cli');
+
+  // Check for the help flag
+  // process.argv[2]
+
+  // Define the flags
+  const flags = {
+    quiet: (process.argv[2] == '-q' || process.argv[2] == '--quiet') ? true : false,
+    silent: (process.argv[2] == '-s' || process.argv[2] == '--silent') ? true : false,
+  }
+
+  // Define the action (serve or deploy)
+  const action = (flags.quiet || flags.silent) ? process.argv[3] : process.argv[2];
+
+  // Define the script to execute
+  const script = (flags.quiet || flags.silent) ? process.argv[4] : process.argv[3];
+
+  // Call the action for the script with the defined flags
+  cli.execute({ action, script, flags });
 
 // Unable to find Restt-CLI
 } catch (error) {
@@ -22,5 +40,5 @@ try {
   console.log('\x1b[2m       npm install -g restt-cli\x1b[0m\n');
   console.log('\x1b[36m  • Install Restt-CLI for this project only');
   console.log('\x1b[2m       npm install restt-cli\x1b[0m\n');
-  console.log('\x1b[34mPlease see \x1b[1mhttps://github.com/larkin-nz/restt-cli#readme\x1b[0m \x1b[34mfor more information\n')
+  console.log('\x1b[34mPlease see \x1b[1mhttps://github.com/resttjs/restt-cli#readme\x1b[0m \x1b[34mfor more information\n')
 }
